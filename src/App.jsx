c0e6c050964d95bf7221d0f6a9a59b224d2517ea
Victor Felicio <victor_felicio@hotmail.com>
2023-04-19 00:00:53 -0300

    create the base of routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import "./index.css";
 
-export function App() {
+import { Outlet } from "react-router-dom";
+
+export const App = () => {
   return (
     <div className="App">
       <h2>Ola Mundo</h2>
+      <Outlet />
     </div>
   );
-}
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,30 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import {App} from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import { App } from "./App";
+import { Home } from "./routes/pages/Home/Home";
+import { Movie } from "./routes/pages/Movie/Movie";
+import { Error } from "./routes/pages/Error/Error";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/home",
+        element: <Home />,
+      },
+      {
+        path: "/movie",
+        element: <Movie />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
